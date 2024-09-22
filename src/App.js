4f0a94ac8fe81a773a728329c18b7943fbd28d98
madalenalamy <89604806+madalenalamy@users.noreply.github.com>
2024-09-22 18:51:25 +0100

    added GitHub

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
-  return <div className="App">Lamy Weather App</div>
+  return (
+    <div className="App">
+      <h1> Weather App </h1>
+      <footer>
+        This project is open-sourced on {" "}
+        <a
+          href="https://github.com/wecodeschool/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }    
      
 
