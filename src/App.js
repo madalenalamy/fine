3dd9b210313cdfc0b6ac4fdd3037a77e0aac8eaa
madalenalamy <89604806+madalenalamy@users.noreply.github.com>
2024-09-22 19:14:19 +0100

    footer changed

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ export default function App() {
     <div className="App">
       <h1> Weather App </h1>
       <footer>
-        This project was coded by Madalena Lamy open-sourced on {" "}
-        <a
-          href="https://github.com/wecodeschool/react-weather-app"
-          target="_blank"
-        >
-          GitHub
-        </a>
+        <p>
+          This project was coded by
+          <a href="/" target="_blank">
+            Madalena Lamy
+          </a>
+          and is open sourced on {" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          and
+          <a href="/" target="_blank">
+            Hosted on Netlify
+          </a>
+        </p>
       </footer>
     </div>
   );
