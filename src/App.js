b34a39df654a247c7c48079f436f4099de818ed6
madalenalamy <89604806+madalenalamy@users.noreply.github.com>
2024-09-22 19:15:13 +0100

    spaces update

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ export default function App() {
       <h1> Weather App </h1>
       <footer>
         <p>
-          This project was coded by
+          This project was coded by{" "}
           <a href="/" target="_blank">
             Madalena Lamy
           </a>
-          and is open sourced on {" "}
+          and is open sourced on{" "}
           <a
             href="https://github.com/wecodeschool/react-weather-app"
             target="_blank"
           >
-            GitHub
+            GitHub{" "}
           </a>
           and
           <a href="/" target="_blank">
